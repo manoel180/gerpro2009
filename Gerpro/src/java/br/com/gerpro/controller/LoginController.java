a9d7b4589804683e16c2ce31983bb803c4396364
marcelo.suporte <marcelo.suporte@a3d67dce-0c25-11de-85a1-fd18384f7567>
2009-03-13 20:29:54 +0000

    Inclusão do controller para logoff

--- Gerpro/src/java/br/com/gerpro/controller/LoginController.java
+++ Gerpro/src/java/br/com/gerpro/controller/LoginController.java
@@ -6,6 +6,7 @@ package br.com.gerpro.controller;
 
 import br.com.gerpro.model.Usuario;
 import br.com.gerpro.service.impl.UsuarioServiceImpl;
+import br.com.gerpro.util.ApplicationSecurityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,9 +24,9 @@ public class LoginController extends MultiActionController {
     public LoginController() {}
 
     public ModelAndView display(HttpServletRequest request, HttpServletResponse response) {
-
+        Usuario usuario = new Usuario();
         ServletRequestDataBinder binder = new ServletRequestDataBinder(usuario);
-        binder.bind(request);
+        binder.bind(request);       
         return new ModelAndView("loginView", "usuario", usuario);
     }
 
@@ -36,16 +37,26 @@ public class LoginController extends MultiActionController {
 
         if(successView.equals("erro")){
             return new ModelAndView("erro");
-        }        
+        }
+       
+        applicationSecurityManager.setUsuario(request, usuario);
         
         successView = "bemVindo" + successView;
         return new ModelAndView(successView);
     }
 
     private UsuarioServiceImpl usuarioService;
-    Usuario usuario = new Usuario();
+
+
 
     public void setUsuarioService(UsuarioServiceImpl usuarioService) {
         this.usuarioService = usuarioService;
     }
+
+    private ApplicationSecurityManager applicationSecurityManager;
+
+    public void setApplicationSecurityManager(ApplicationSecurityManager applicationSecurityManager) {
+        this.applicationSecurityManager = applicationSecurityManager;
+    }
+
 }
