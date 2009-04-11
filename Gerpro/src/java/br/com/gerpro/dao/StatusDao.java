4e27035e54a44a3e8fa3fe60ebd6f57579a4b62e
marcelo.suporte@gmail.com <marcelo.suporte@gmail.com@a3d67dce-0c25-11de-85a1-fd18384f7567>
2009-04-11 22:26:24 +0000

    Inclusão da classe StatusDao.java

---
+++ Gerpro/src/java/br/com/gerpro/dao/StatusDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.gerpro.dao;
+
+import br.com.gerpro.model.Status;
+
+/**
+ *
+ * @author M3R
+ */
+public interface StatusDao extends GenericDao<Status,Integer>{
+
+}
